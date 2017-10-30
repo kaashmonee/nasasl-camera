af229268b9df4f76bc142a8d3f5f5be5425fc027
kaashmonee <skandakk@gmail.com>
2017-10-30 01:41:07 -0400

    added notes

--- detect_color.py
+++ detect_color.py
@@ -5,38 +5,44 @@ import argparse
 import cv2
 
 #constructing argument parse and parsing command line arguments
-path = "temp path 2 image"
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = path)
-args = vars(ap.parse_args())
 
-#loading the image
-image = cv2.imread(args["image"])
+def main():
+    print("Changes workingular madam.")
+    path = "temp path 2 image"
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", help = path)
+    args = vars(ap.parse_args())
 
-##defining list of boundaries
-#the arrays read backwards and say that RED is from RGB 100, 200 in the first 
-#array
+    #loading the image
+    image = cv2.imread(args["image"])
 
-#of course, we can adjust these boundaries as we like 
-boundaries = [
-    ([17, 15, 100], [50, 56, 200]),
-    ([86, 31, 4], [220, 88, 50]),
-    ([25, 146, 190], [62, 174, 250]),
-    ([103, 86, 65], [145, 133, 128])
-]
+    ##defining list of boundaries
+    #the arrays read backwards and say that RED is from RGB 100, 200 in the first 
+    #array
+
+    #of course, we can adjust these boundaries as we like 
+    boundaries = [
+        ([17, 15, 100], [50, 56, 200]),
+        ([86, 31, 4], [220, 88, 50]),
+        ([25, 146, 190], [62, 174, 250]),
+        ([103, 86, 65], [145, 133, 128])
+    ]
 
 
-for (lower, upper) in boundaries:
-    #creating np arrays from the boundaries
-    lower = np.array(lower, dtype = "uint8")
-    upper = np.array(upper, dtype = "uint8")
+    for (lower, upper) in boundaries:
+        #creating np arrays from the boundaries
+        lower = np.array(lower, dtype = "uint8")
+        upper = np.array(upper, dtype = "uint8")
 
-    #finds the colors within the specified boundaries and aplies the mask
-    mask = cv2.inRange(image, lower, upper)
-    output = cv2.bitwise_and(image, image, mask = mask)
+        #finds the colors within the specified boundaries and aplies the mask
+        mask = cv2.inRange(image, lower, upper)
+        output = cv2.bitwise_and(image, image, mask = mask)
 
-    #show the images
-    cv2.imshow("images", np.hstack([image, output]))
-    cv2.waitkey(0)
+        #show the images
+        cv2.imshow("images", np.hstack([image, output]))
+        cv2.waitkey(0)
 
-#IMPROVE DOCUMENTATION AND TESTS
+    #IMPROVE DOCUMENTATION AND TESTS
+
+if __name__ == "__main__":
+    main()
